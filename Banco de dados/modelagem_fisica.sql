a88b929d5c124a8f280e379ac142de74563b8ee5
rhian-bottura <61800027+rhian-bottura@users.noreply.github.com>
2020-04-03 16:36:55 -0300

    atualizaçao
    tentativa de organizar o git hub

--- Banco de dados/modelagem_fisica.sql
+++ Banco de dados/modelagem_fisica.sql
@@ -1,10 +1,14 @@
 create database luminous;
 use luminous;
 create table usuario 
-(cnpj varchar(16) primary key,
+(
+id_empresa int primary key,
+cnpj varchar(16),
 nome varchar(50) ,
 senha varchar(20),
-email varchar(50)
+email varchar(50),
+tel1 varchar(12),
+tel2 varchar(12)
 );
 
 create table filial (
@@ -14,23 +18,24 @@ estado varchar(2),
 bairro varchar(30),
 cidade varchar(30),
 rua varchar(50),
-fk_cnpj_usuario  varchar(16),
-   foreign key (fk_cnpj_usuario) references usuario(cnpj)
-);
+fk_usuario int,
+   foreign key (fk_usuario) references usuario(id_empresa)
+)auto_increment=100;
 
 create table comodo (
 id_comodo int primary key auto_increment,
 nome varchar(30),
 qtde_lampadas int,
 fk_filial int,
-foreign key (fk_filial) references filial(id_filial)) auto_increment = 100;
+num_sensores int,
+foreign key (fk_filial) references filial(id_filial)) auto_increment = 1000;
 
 create table sensor (
 id_sensor int primary key auto_increment,
 grau_luminosidade float,
 fk_comodo int,
 foreign key (fk_comodo) references comodo(id_comodo)
-) auto_increment = 1000;
+) auto_increment = 10000;
 
 drop database luminous;
 
