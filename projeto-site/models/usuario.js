1b54ed511f6b6b7bd24dbb8697a6637f9c9069be
Cesar-Almeida <cesar.almeida@bandtec.com.br>
2020-06-15 15:49:35 -0300

    Aula 15/06 Yoshi Conecção com o banco

--- projeto-site/models/usuario.js
+++ projeto-site/models/usuario.js
@@ -8,29 +8,54 @@ https://codewithhugo.com/sequelize-data-types-a-practical-guide/
 module.exports = (sequelize, DataTypes) => {
     let Usuario = sequelize.define('Usuario',{
 		id: {
-			field: 'id',
+			field: 'idUsuario',
 			type: DataTypes.INTEGER,
 			primaryKey: true,
 			autoIncrement: true
 		},		
 		nome: {
-			field: 'nome',
+			field: 'Nome',
 			type: DataTypes.STRING,
 			allowNull: false
 		},
 		login: {
-			field: 'login',
+			field: 'Email',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		tipousuario: {
+			field: 'TipoUsuario',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		cpf: {
+			field: 'CPF',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		rg: {
+			field: 'RG',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		datanasc: {
+			field: 'DataNasc',
 			type: DataTypes.STRING,
 			allowNull: false
 		},
 		senha: {
-			field: 'senha',
+			field: 'Senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		empresa: {
+			field: 'fkEmpresa',
 			type: DataTypes.STRING,
 			allowNull: false
 		}
 	}, 
 	{
-		tableName: 'usuario', 
+		tableName: 'Usuario', 
 		freezeTableName: true, 
 		underscored: true,
 		timestamps: false,
--- projeto-site/routes/leituras.js
+++ projeto-site/routes/leituras.js
@@ -12,11 +12,11 @@ router.get('/ultimas', function(req, res, next) {
 	console.log(`Recuperando as últimas ${limite_linhas} leituras`);
 	
 	const instrucaoSql = `select top ${limite_linhas} 
-						temperatura, 
-						umidade, 
-						momento,
-						FORMAT(momento,'HH:mm:ss') as momento_grafico 
-						from leitura order by id desc`;
+						IdEventos,
+						GrauLum, 
+						DataEvento,
+						FORMAT(DataEvento,'HH:mm:ss') as momento_grafico
+						from Eventos order by IdEventos desc`;
 
 	sequelize.query(instrucaoSql, {
 		model: Leitura,
@@ -37,8 +37,11 @@ router.get('/tempo-real', function (req, res, next) {
 	
 	console.log(`Recuperando a última leitura`);
 
-	const instrucaoSql = `select top 1 temperatura, umidade, FORMAT(momento,'HH:mm:ss') as momento_grafico  
-						from leitura order by id desc`;
+	const instrucaoSql = `select top 1 IdEventos,
+						GrauLum, 
+						DataEvento,
+						FORMAT(DataEvento,'HH:mm:ss') as momento_grafico
+						from Eventos order by IdEventos desc`;
 
 	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
 		.then(resultado => {
@@ -57,13 +60,10 @@ router.get('/estatisticas', function (req, res, next) {
 	console.log(`Recuperando as estatísticas atuais`);
 
 	const instrucaoSql = `select 
-							max(temperatura) as temp_maxima, 
-							min(temperatura) as temp_minima, 
-							avg(temperatura) as temp_media,
-							max(umidade) as umidade_maxima, 
-							min(umidade) as umidade_minima, 
-							avg(umidade) as umidade_media 
-						from leitura`;
+							max(GrauLum) as lum_maxima, 
+							avg(GrauLum) as lum_média,
+							min(GrauLum) as lum_minima
+						from Eventos`;
 
 	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
 		.then(resultado => {
