606188c8211c3dac8bc643555579063c433f3d28
rhian-bottura <61800027+rhian-bottura@users.noreply.github.com>
2020-06-27 00:49:02 -0300

    Função Enviar Email feita(Rota)

--- projeto-site/routes/usuarios.js
+++ projeto-site/routes/usuarios.js
@@ -3,8 +3,43 @@ var router = express.Router();
 var sequelize = require('../models').sequelize;
 var Usuario = require('../models').Usuario;
 var Empresa = require('../models').Empresa;
+var Filial = require('../models').Filial
+var nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({ 
+    host:"SMTP.office365.com",
+    port:"587",
+    secure:false,
+    auth:{
+        user:"201grupo2c@bandtec.com.br",
+        pass:"Luminous@@2020"
+    }
+}
+)
+
 
 let sessoes = [];
+// enviar email da luminous
+router.post('/enviarEmail',function(req,res,nexy){
+    
+    console.log('Enviando...')
+    var email = req.body.email
+    transporter.sendMail(
+        {
+            from:"Luminous <201grupo2c@bandtec.com.br>",
+            to:email,
+            subject:"Novidades Luminous",
+            text:"Ola, Bem vindo a Luminous, agora você recebera todas as novidades e promoções da empresa atraves deste email,nosso intuito é sua confortabilidade ao usar nosso produto, afim de economizar tempo e facilitar sua vida.Obrigado por contar com a gente, Assinado:Equipe de TI Luminous!"
+        }
+    ).then(message =>{
+        console.log(message)
+    }).catch(err =>{
+        console.log(err)
+    })
+    console.log(req.body.email)
+
+  
+})
 
 /* Recuperar usuário por login e senha */
 router.post('/autenticar', function(req, res, next) {
@@ -12,8 +47,8 @@ router.post('/autenticar', function(req, res, next) {
 
 	var email = req.body.emailLogin; // depois de .body, use o nome (name) do campo em seu formulário de login
 	var senha = req.body.senhaLogin; // depois de .body, use o nome (name) do campo em seu formulário de login	
-	email.trim()
-	senha.trim()
+	email = email.trim()
+	senha = senha.trim()
 	let instrucaoSql = `select * from usuario where Email='${email}' and Senha='${senha}'`;
 	console.log(instrucaoSql);
 
@@ -74,39 +109,44 @@ router.post('/cadastrar', function(req, res, next) {
 	
 });
 //cadastrar filial
-router.post('/filial',function(res,req,nexy){
+router.post('/filial',function(req,res,nexy){
 	console.log('Criando filial');
+
 	Usuario.create({
-		nome : req.body.nome,
-		email : req.body.email,
-		tipousuario: "G",
-		cpf: req.body.cpf,
-		rg: req.body.rg,
-		datanasc: req.body.dataNasc,
-		senha: req.body.senha,
-		fkcnpj:req.body.fkCnpj
-		}).then(resultado => {
-		console.log(`Registro criado: ${resultado}`)
+        nome : req.body.nome,
+        email : req.body.email,
+        tipousuario: "G",
+        cpf: req.body.cpf,
+        rg: req.body.rg,
+        datanasc: req.body.dataNasc,
+        senha: req.body.senha,
+        fkcnpj:req.body.fkCnpj
+        }).then(resultado => {
+        console.log(`Registro criado: ${resultado}`)
         res.send(resultado);
     }).catch(erro => {
-		console.error(erro);
-		res.status(500).send(erro.message);
-	  });
-	  Filial.create({
-		  telefone: req.body.telefone,
-		  rua: req.body.rua,
+        console.error(erro);
+        res.status(500).send(erro.message);
+      });
+      Filial.create({
+          telefone: req.body.telefone,
+          rua: req.body.rua,
 		  bairro: req.body.bairro,
-		  cidade: req.body.cidade,
-		  numero: req.body.numero,
-		  fkcnpj: req.body.fkCnpj,
-		  fkusuario: Usuario.Usuario
-	  }).then(resultado =>{
-		  console.log(`Registro criado: ${resultado}`)
-		  res.send(resultado);
-	  }).catch(erro => {
-		console.error(erro);
-		res.status(500).send(erro.message);
-	  });
+		  estado:"sp",
+          cidade: req.body.cidade,
+          numero: req.body.numero,
+          fkcnpj: req.body.fkCnpj,
+          fkusuario: Usuario.Usuario
+      }).then(resultado =>{
+          console.log(`Registro criado: ${resultado}`)
+          res.send(resultado);
+      }).catch(erro => {
+        console.error(erro);
+        res.status(500).send(erro.message);
+      });
+
+														
+	 
 
 })
 
