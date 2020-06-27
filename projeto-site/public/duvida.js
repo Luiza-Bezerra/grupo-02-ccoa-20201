ef14f7e7270db127d58a362966ab6b00179983c0
rhian-bottura <61800027+rhian-bottura@users.noreply.github.com>
2020-06-27 00:50:43 -0300

    criando o script de duvidas
    script de duvidas para conseguir colocar essa função em varias telas, e ajustes no html do cadastro de filial

---
+++ projeto-site/public/duvida.js
@@ -0,0 +1,23 @@
+function enviarDuvida(){
+    var formulario = new URLSearchParams(new FormData(form_email));
+    fetch("/usuarios/enviarEmail", {
+          method: "POST",
+          body: formulario
+      }).then(function (response) {
+          
+          if (response.ok) {
+
+             alert('Agora irar Receber as Novidades da Luminous em seu email')
+            
+
+          } else {
+
+              console.log('Erro de cadastro!');
+              response.text().then(function (resposta) {
+                  console.log(resposta)
+              });
+         
+          }
+      });
+
+      return false;
